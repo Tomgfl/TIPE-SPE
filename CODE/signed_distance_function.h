aeaaa9688491e62f8c33b95fe8c54069074519b8
Eliot <eliot.burgalat@gmail.com>
2023-12-01 13:35:51 +0100

    ajout d'opérations

--- CODE/signed_distance_function.h
+++ CODE/signed_distance_function.h
@@ -26,8 +26,6 @@ float SDF_Pyramide(coord p, coord centre, float H, float c);
 float SDF_plan(coord p, vector n, float h);
 float SDF_Tor(coord p, coord centre, float R, float r);
 
-float MIN_ALL_SDF(coord pts);
-
 // Rotations
 coord rotation_x(coord v, float angle);
 coord rotation_y(coord v, float angle);
